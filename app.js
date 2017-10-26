0e8b160f647c34df36fab00dd3186303c5b1dc1c
Nguyễn Ngói <ngoi.nguyen@divui.com>
2017-10-26 19:54:13 +0700

    openid first

---
+++ app.js
@@ -0,0 +1,14 @@
+const Provider = require('oidc-provider');
+const PORT = process.env.PORT || 3000;
+const APP_NAME = process.env.HEROKU_APP_NAME || 'localhost';
+
+const oidc = new Provider(`https://${APP_NAME}.herokuapp.com`);
+console.log(APP_NAME);
+
+oidc.initialize({
+  clients: [{client_id: '', client_secret: 'bar', redirect_uri: `http://localhost:8080`}]
+}).then(() => {
+  oidc.app.proxy = true;
+  oidc.app.keys = '';
+  oidc.app.listen(PORT);
+});

956583fc138f231ad50fd02bede7b6dd8fa69d88
Nguyễn Ngói <ngoi.nguyen@divui.com>
2017-10-26 20:00:34 +0700

    add app name

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const Provider = require('oidc-provider');
 const PORT = process.env.PORT || 3000;
-const APP_NAME = process.env.HEROKU_APP_NAME || 'localhost';
+const APP_NAME = process.env.HEROKU_APP_NAME || 'immense-forest-13455';
 
 const oidc = new Provider(`https://${APP_NAME}.herokuapp.com`);
 console.log(APP_NAME);
